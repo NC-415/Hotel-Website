8c313d9f466433063df8fcd6d75c2cfcaa7bc82f
Govindu Thejana <149306686+Govindu-Thejana@users.noreply.github.com>
2024-08-13 19:25:40 +0530

    Initial Project Setup

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://govindu:56692942@hotel-website-softwarep.qlji5.mongodb.net/?retryWrites=true&w=majority&appName=Hotel-Website-SoftwareProject";
