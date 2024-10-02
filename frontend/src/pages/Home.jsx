a4f3aaccaed695622c85c78486d1afd7e1cc6619
pasan <14992pasan@gmail.com>
2024-10-02 23:54:15 +0530

    Add google map

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -21,29 +21,11 @@ const Home = () => {
 
   return (
     <div className="bg-gray-100">
-      {/* Header */}
-      <header className="bg-black text-white">
-        <div className="container mx-auto flex justify-between items-center p-4">
-          <div className="logo">
-            <img src="/images/Logo.jpg" alt="Hotel Logo" className="w-24 h-10 object-cover"/>
-          </div>
-          <nav className="flex space-x-4">
-            <a href="#" className="hover:text-green-400">Home</a>
-            <a href="#" className="hover:text-green-400">Weddings</a>
-            <a href="#" className="hover:text-green-400">Accommodation</a>
-            <a href="#" className="hover:text-green-400">Events</a>
-            <a href="#" className="hover:text-green-400">Offers</a>
-            <a href="#" className="hover:text-green-400">Contact Us</a>
-          </nav>
-          <div>
-            <button className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded">Book Now</button>
-          </div>
-        </div>
-      </header>
+
 
       {/* Hero Section */}
       <section className="relative">
-        <img src="/images/bg.jpg" alt="Hotel Exterior" className="w-full h-96 object-cover" />
+        <img src="/images/bg.jpg" alt="Hotel Exterior" className="w-full h-128 object-cover" />
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <div className="text-center text-white">
             <h2 className="text-3xl font-bold mb-4">Your lavish home away from home</h2>
@@ -61,7 +43,7 @@ const Home = () => {
           <div className="md:w-1/3 p-6 flex flex-col justify-center">
             <h2 className="text-3xl font-serif mb-4 text-gray-800">Movement of elegance!</h2>
             <p className="text-gray-600 mb-6">At Hotel Somewhere, we offer a variety of accommodation options to cater to both leisure travelers and business professionals alike.</p>
-            <button className="bg-blue-900 text-white py-2 px-4 hover:bg-blue-900 transition duration-300">BOOK NOW</button>
+            <button className="bg-scolor text-white py-2 px-4 hover:bg-pcolor transition duration-300">BOOK NOW</button>
           </div>
         </div>
       </section>
@@ -76,21 +58,21 @@ const Home = () => {
             <img src="/images/all.jpg" alt="Golden Package" className="w-full h-48 object-cover" />
             <div className="p-6">
               <h3 className="text-2xl font-bold mb-2">Golden Package</h3>
-              <a href="#" className="text-blue-900 mt-4 block">Find out more...</a>
+              <a href="#" className="text-pcolor mt-4 block">Find out more...</a>
             </div>
           </div>
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
             <img src="/images/all.jpg" alt="Premium Package" className="w-full h-48 object-cover" />
             <div className="p-6">
               <h3 className="text-2xl font-bold mb-2">Premium Package</h3>
-              <a href="#" className="text-blue-900 mt-4 block">Find out more...</a>
+              <a href="#" className="text-pcolor mt-4 block">Find out more...</a>
             </div>
           </div>
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
             <img src="/images/all.jpg" alt="Silver Package" className="w-full h-48 object-cover" />
             <div className="p-6">
               <h3 className="text-2xl font-bold mb-2">Silver Package</h3>
-              <a href="#" className="text-blue-900 mt-4 block">Find out more...</a>
+              <a href="#" className="text-pcolor mt-4 block">Find out more...</a>
             </div>
           </div>
         </div>
@@ -99,7 +81,7 @@ const Home = () => {
 {/* Carousel */}
 <section className="container mx-auto py-12">
         <div className="relative">
-          <div className="h-128 overflow-hidden rounded-lg">
+          <div className="h-96 overflow-hidden rounded-lg">
             <img src={`/images/${currentSlide === 0 ? 'bride.jpg' : currentSlide === 1 ? 'all.jpg' : 'bg.jpg'}`} alt="Hotel" className="w-full h-full object-cover" />
           </div>
 
@@ -124,14 +106,14 @@ const Home = () => {
       <div class="relative flex flex-col items-center mx-auto lg:flex-row-reverse lg:max-w-5xl lg:mt-12 xl:max-w-6xl">
 
     
-    <div class="w-full h-64 lg:w-2/3 lg:h-auto">
+    <div class="w-full h-80 lg:w-3/4 lg:h-auto">
         <img class="h-full w-full object-cover" src="/images/hotel-room.jpg" alt="our rooms"/>
     </div>
     <div
         class="max-w-lg bg-white md:max-w-2xl md:z-10 md:shadow-lg md:absolute md:top-0 md:mt-48 lg:w-3/5 lg:left-0 lg:mt-20 lg:ml-20 xl:mt-24 xl:ml-12">
         
         <div class="flex flex-col p-12 md:px-16">
-            <h2 class="text-2xl font-medium uppercase text-blue-800 lg:text-4xl">our room & Suite choices</h2>
+            <h2 class="text-2xl font-medium uppercase text-pcolor lg:text-4xl">our room & Suite choices</h2>
             <p class="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -140,7 +122,7 @@ const Home = () => {
             
             <div class="mt-8">
                 <a href="#"
-                    class="inline-block w-full text-center text-lg font-medium text-gray-100 bg-blue-900 border-solid border-2 border-gray-600 py-4 px-10 hover:bg-green-800 hover:shadow-md md:w-48">Read
+                    class="inline-block w-full text-center text-lg font-medium text-gray-100 bg-scolor border-solid border-2 border-gray-600 py-4 px-10 hover:bg-pcolor hover:shadow-md md:w-48">Read
                     More</a>
             </div>
         </div>
@@ -160,7 +142,7 @@ const Home = () => {
             <div className="p-6">
               <h3 className="text-2xl font-bold mb-2">Single Room</h3>
               <p className="text-gray-600">Comfort and convenience for solo travelers.</p>
-              <a href="#" className="text-green-500 mt-4 block">Learn More</a>
+              <a href="#" className="text-pcolor mt-4 block">Learn More</a>
             </div>
           </div>
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
@@ -168,7 +150,7 @@ const Home = () => {
             <div className="p-6">
               <h3 className="text-2xl font-bold mb-2">Double Room</h3>
               <p className="text-gray-600">Ideal for couples.</p>
-              <a href="#" className="text-green-500 mt-4 block">Learn More</a>
+              <a href="#" className="text-pcolor mt-4 block">Learn More</a>
             </div>
           </div>
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
@@ -176,7 +158,7 @@ const Home = () => {
             <div className="p-6">
               <h3 className="text-2xl font-bold mb-2">Suite</h3>
               <p className="text-gray-600">The ultimate in luxury and space.</p>
-              <a href="#" className="text-green-500 mt-4 block">Learn More</a>
+              <a href="#" className="text-pcolor mt-4 block">Learn More</a>
             </div>
           </div>
         </div>
@@ -204,7 +186,7 @@ const Home = () => {
             <p cclassName="text-base text-gray-500 leading-relaxed">Embark on a culinary journey and discover a world of delectable dishes from Sri Lanka and beyond. 
               Immerse yourself in the exquisite flavours and let your taste buds explode with every bite. 
               Head to our cosy restaurants and indulge in a wide range of freshly prepared dishes or beverages of your choice.</p>
-            <p><a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Find out more</a></p>
+            <p><a href="#" class="text-white bg-scolor hover:bg-pcolor focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-scolor dark:hover:bg-pcolor focus:outline-none dark:focus:ring-scolor">Find out more</a></p>
           </div>
         </div>
       </div>
@@ -218,21 +200,21 @@ const Home = () => {
             <img src="/images/chiken.jpg" alt="Single Room" className="w-full h-48 object-cover" />
             <div className="p-6">
               <h3 className="text-2xl font-bold mb-2">CHIKEN FLAVOURS</h3>
-              <a href="#" class="text-white bg-blue-900 hover:bg-blue-950 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-800 dark:hover:bg-blue-800 focus:outline-none dark:focus:ring-blue-800">$2 P/PORTION</a>
+              <a href="#" class="text-white bg-scolor hover:bg-pcolor focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-scolor dark:hover:bg-pcolor focus:outline-none dark:focus:ring-pcolor">$2 P/PORTION</a>
             </div>
           </div>
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
           <img src="/images/chiken.jpg" alt="Single Room" className="w-full h-48 object-cover" />
             <div className="p-6">
               <h3 className="text-2xl font-bold mb-2">CHIKEN FLAVOURS</h3>
-              <a href="#" class="text-white bg-blue-900 hover:bg-blue-950 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-800 dark:hover:bg-blue-800 focus:outline-none dark:focus:ring-blue-800">$2 P/PORTION</a>
+              <a href="#" class="text-white bg-scolor hover:bg-pcolor focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-scolor dark:hover:bg-pcolor focus:outline-none dark:focus:ring-pcolor">$2 P/PORTION</a>
             </div>
           </div>
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
           <img src="/images/chiken.jpg" alt="Single Room" className="w-full h-48 object-cover" />
             <div className="p-6">
               <h3 className="text-2xl font-bold mb-2">CHIKEN FLAVOURS</h3>
-              <a href="#" class="text-white bg-blue-900 hover:bg-blue-950 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-800 dark:hover:bg-blue-800 focus:outline-none dark:focus:ring-blue-800">$2 P/PORTION</a>
+              <a href="#" class="text-white bg-scolor hover:bg-pcolor focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-scolor dark:hover:bg-pcolor focus:outline-none dark:focus:ring-pcolor">$2 P/PORTION</a>
             </div>
           </div>
         </div>
@@ -271,7 +253,18 @@ const Home = () => {
           </div>
         </div>
       </section>
-      
+      <p className="flex justify-center items-center min-h-screen">
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15362.633136406359!2d80.10702796495386!3d7.7688024577783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afcd610a5b4ea93%3A0xefc5f088590da77d!2sSuneragira%20Reception%20Hall!5e0!3m2!1sen!2slk!4v1727886479261!5m2!1sen!2slk"
+    width="1280"
+    height="600"
+    style={{ border: 0 }}
+    allowFullScreen=""
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  ></iframe>
+</p>
+
     </div>
     
   );
